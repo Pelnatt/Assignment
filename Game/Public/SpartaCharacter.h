aa933b4359a700ec95dd30b5ec0a8c75a08b9dba
Pelnatt <165894339+Pelnatt@users.noreply.github.com>
2025-02-05 12:37:19 +0900

    Add files via upload

---
+++ Game/Public/SpartaCharacter.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SpartaCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+struct FInputActionValue;
+
+UCLASS()
+class GAME_API ASpartaCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASpartaCharacter();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	USpringArmComponent* SpringArmComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* CameraComp;
+
+protected:
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void Move(const FInputActionValue& value);
+	UFUNCTION()
+	void StartJump(const FInputActionValue& value);
+	UFUNCTION()
+	void StopJump(const FInputActionValue& value);
+	UFUNCTION()
+	void Look(const FInputActionValue& value);
+	UFUNCTION()
+	void StartSprint(const FInputActionValue& value);
+	UFUNCTION()
+	void StopSprint(const FInputActionValue& value);
+
+private:
+	float NormalSpeed;
+	float SprintSpeedMultiplier;
+	float SprintSpeed;
+};
